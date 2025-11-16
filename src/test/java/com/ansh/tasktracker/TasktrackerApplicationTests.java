3e6c38b3e8e234d9a505b1b0005cdac00cce84bd
Anshul Shrivastava <anshulshrivastava21cent@gmail.com>
2025-11-16 17:16:47 +0530

    Initial project setup

---
+++ src/test/java/com/ansh/tasktracker/TasktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansh.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
